639e97c912d046a357528aa4702edac681b1199c
Bipon Roy <biponroy400@gmail.com>
2023-09-12 18:34:29 +0600

    Added New Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import "./App.css";
+import Bookmarks from "./components/Bookmarks/Bookmarks";
+import Blogs from "./components/Blogs/Blogs";
 import Header from "./components/header/Header";
+import { useState } from "react";
 
 function App() {
+    const [bookmarks, setBookmarks] = useState([]);
+
+    const handleAddToBookmark = (blog) => {
+        console.log("Bookmark");
+    };
+
     return (
-        <>
+        <div className=" mx-auto max-w-7xl">
             <Header></Header>
-        </>
+            <div className="md:flex justify-between gap-6">
+                <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
+                <Bookmarks></Bookmarks>
+            </div>
+        </div>
     );
 }
 
---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+import { FaBookmark } from "react-icons/fa";
+
+const Blog = ({ blog, handleAddToBookmark }) => {
+    const { title, cover, reading_time, author, author_img, posted_date, hashtags } = blog;
+    return (
+        <div className="mb-12">
+            <img
+                className="w-full rounded-md"
+                src={cover}
+                alt={`Cover picture of the title ${title}`}
+            />
+            <div className="flex justify-between">
+                <div>
+                    <img className="w-14" src={author_img} alt="" />
+                    <div className="">
+                        <h3 className="text-2xl">{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div className="">
+                    <span>{reading_time} min read</span>
+                    <button className="text-2xl" onClick={handleAddToBookmark}>
+                        <FaBookmark />
+                    </button>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {hashtags.map((hash, idx) => (
+                    <span key={idx}>
+                        <a>#{hash}</a>
+                    </span>
+                ))}
+            </p>
+        </div>
+    );
+};
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired,
+};
+export default Blog;
